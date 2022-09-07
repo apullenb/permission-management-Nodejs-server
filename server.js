3f1f03c5a2a87322d35fe2a770e021309af7678b
apullenb <apullenb@gmail.com>
2022-09-07 11:28:38 -0500

    implement role auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const { users } = require('./data')
-const { authUser } = require('./basicAuth')
+const { ROLE, users } = require('./data')
+const { authUser, authRole } = require('./basicAuth')
 const projectRouter = require('./routes/projectRoutes')
 
 
@@ -17,7 +17,7 @@ app.get('/dashboard', authUser, (req, res) => {
   res.send('Dashboard Page')
 })
 
-app.get('/admin', (req, res) => {
+app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
   res.send('Admin Page')
 })
 
